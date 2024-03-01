691f233842530b195e6d1610a5f9c4e6c67bcc39
filiprendon <frendont2223@politecnics.barcelona>
2024-03-01 16:57:26 +0100

    Borrar cookies

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let InfoUsuario = document.cookie;
+let botonBorrar = document
 function EnmagatzemaCookie(){
     let nombreUsuario = document.getElementById('nombre').value;
     let email = document.getElementById('email').value;
@@ -13,9 +14,21 @@ function recuperarCookie(){
     if(InfoUsuario){
         let datos = InfoUsuario.split(" ");
         // console.error("hola", datos);
-        document.getElementById('recuperar').innerHTML = datos;
+        document.getElementById('recuperar').innerHTML = JSON.stringify(datos);
     }else{
-        console.log("Hola no tengo tus datos aún");
+        alert("Hola, no tengo tus datos aún, por favor pon tu nombre y email");
     }
 
+}
+
+function eliminarCookie(){
+    if(InfoUsuario !== ''){
+        alert("Voy a eliminar estas cookies y no hay nada que puedas hacer");
+        document.cookie = "usuario= ; max-age=-1";
+        document.cookie = "email= ; max-age=-1";
+        location.reload();
+    } else {
+        alert("No tienes ninguna cookie para borrar");
+    }
+    
 }
